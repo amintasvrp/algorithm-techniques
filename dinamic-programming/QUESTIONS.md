e4d945dd07ac42a02933d30e174af75f4b8aa640
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2019-11-07 22:10:10 -0300

    Questao 1

---
+++ dinamic-programming/QUESTIONS.md
@@ -0,0 +1,12 @@
+# Questões
+
+## Soma do subconjunto
+
+Dado um conjunto de números inteiros não negativos e um valor sum, implemente uma solução, usando programação dinâmica, que determina se existe um subconjunto, do conjunto dado, cuja soma dos valores é igual a sum.
+
+Exemplo:
+
+- Entrada: `{3, 34, 4, 12, 5, 2}`, `sum = 9`
+  - Saída: `True` // o subconjunto é `(4,5)`
+- Entrada: `{8, 34, 4, 12,5, 1}`, `sum = 7`
+  - Saída: `False`
---
+++ dinamic-programming/soma-do-subconjunto.py
@@ -0,0 +1,53 @@
+def matrix(conj, sum):
+    conj.append(0)
+    conj = sorted(conj)
+
+    matrix = []
+    for x in range(len(conj)):
+        matrix.append([])
+
+    for i in range(len(conj)):        
+        matrix[i].append(0)
+    
+    for j in range(sum):
+        matrix[0].append(0)
+    
+    for i in range(1, len(conj)):
+        for j in range(1, sum + 1):
+            if(conj[i] > j):
+                matrix[i].append(matrix[i-1][j])
+            else:
+                matrix[i].append(max(matrix[i-1][j], conj[i] + matrix[i-1][j-(conj[i])]))
+    
+    
+    return matrix
+
+def temSubconj(conj, sum):
+    result = matrix(conj, sum)
+    return result[-1][-1] == sum
+
+
+
+def subconj(matrix, conj):
+    i = len(matrix) - 1
+    j = len(matrix[-1]) - 1
+    conj = sorted(conj)
+
+    subconj = []
+
+
+    while(matrix[i][j] != 0):
+        if(matrix[i][j] == matrix[i-1][j]):
+            i -= 1
+        else:
+            subconj.append(conj[i])
+            j = j - conj[i]
+            i = i - 1
+    
+    return subconj
+
+
+print(temSubconj([3, 34, 4, 12, 5, 2], 9))
+m = matrix([3, 34, 4, 12, 5, 2], 9)
+print(subconj(m, [3, 34, 4, 12, 5, 2]))
+print(temSubconj([8, 34, 4, 12, 5, 1], 7))
